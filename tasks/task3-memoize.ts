39071e4174308d62764c0ec429bf5a49dab70773
nikshep <klimenko.leva.lol2@yandex.ru>
2026-03-15 00:24:22 +0800

    migrate task-3 to ts

--- tasks/task3-memoize.ts
+++ tasks/task3-memoize.ts
@@ -5,27 +5,32 @@
 - Аргументы функции — только строки или числа (для упрощения)
 - Кэшируйте результат по аргументам
 */
+type MemoFn<This, Args extends unknown[], R> = (this: This, ...args: Args) => R;
 
-function memoize(fn) {
-  const cache = new Map();
+function memoize<This, Args extends unknown[], R>(
+  fn: MemoFn<This, Args, R>,
+): MemoFn<This, Args, R> {
+  const cache = new Map<string, R>();
 
-  return function (...args) {
+  return function (this: This, ...args: Args): R {
     const key = JSON.stringify(args);
 
     if (cache.has(key)) {
-      return cache.get(key);
+      return cache.get(key)!;
     }
     const result = fn.apply(this, args);
+
     cache.set(key, result);
     return result;
   };
 }
 
-const slowAdd = (a, b) => {
+const slowAdd = (a: number, b: number) => {
   return a + b;
 };
 
 const memoAdd = memoize(slowAdd);
+
 const res1 = memoAdd(1, 2); // возвращает 3
 const res2 = memoAdd(1, 2); // из кэша, возвращает 3
 console.log(res1);
